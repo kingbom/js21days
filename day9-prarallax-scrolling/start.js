7b8824919c215765a0db99bc66c25003a29d51b8
jaruwit <kingbom_300432@hotmail.com>
2021-07-07 23:44:19 +0700

    [DAY 9] Parallax Scrolling

---
+++ day9-prarallax-scrolling/start.js
@@ -0,0 +1,15 @@
+(() => {
+
+  const onScroll = () => {
+    const moonElem = document.querySelector(".moon");
+    const wishElem = document.querySelector(".wish");
+    moonElem.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`;
+    wishElem.style.transform = `translateY(${window.scrollY * -1.2}%)`;
+  }
+
+  const run = () => {
+    document.addEventListener('scroll', onScroll);
+  }
+
+  run();
+})();
